615a13fff59a4eac09b7c058a74658f56cc9a6f0
Granddddddd <131579488+Granddddddd@users.noreply.github.com>
2023-12-23 13:03:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+let numberX = +prompt('Яке число ви хочете дізнатися?')
+switch (numberX) {
+    case 1:
+    case 10:
+    case 28:
+    case 32:
+    case 44:
+    case 68:
+    case 79:
+    case 86:
+    case 97:
+    case 100:
+    case 226:
+    case 331:
+    case 404:
+    case 565:
+    case 665:
+    case 748:
+    case 888:
+    case 989:
+    alert("Щасливі числа");
+    break;
+    case 13:
+    case 666:
+    case 4:
+    case 3:
+    alert("Нещасливі числа");
+    break;
+    default:
+    alert("Такого ще не має в моему довіднику")
+}
